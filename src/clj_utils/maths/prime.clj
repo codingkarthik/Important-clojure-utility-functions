bc374ee121167689dda377baffa3506bc1759ef6
Karthikeyan Chinnakonda <kartthikspiky@gmail.com>
2018-05-21 16:17:43 +0530

    Interpolate the cond in prime? with spaces

--- src/clj_utils/maths/prime.clj
+++ src/clj_utils/maths/prime.clj
@@ -3,9 +3,12 @@
 (defn prime? [x]
   (loop [i 2]
     (cond
-      (or (= x 0)
-          (= x 1)) false
+
+      (or (= x 0) (= x 1)) false
+
       (> (* i i) x) true
+
       (zero? (mod x i)) false
+
       :else
       (recur (inc i)))))
